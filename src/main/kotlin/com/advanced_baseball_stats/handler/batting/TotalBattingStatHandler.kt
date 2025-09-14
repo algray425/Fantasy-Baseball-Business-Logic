d8e15dd96f78784eda22d107935525420d711087
grayengineering425 <32369730+grayengineering425@users.noreply.github.com>
2025-09-13 18:20:09 -0700

    Remove Duplicate Code from TotalBattingStatHandler
    Removing getCurrentDate function and replacing it with common DateHelper.

--- src/main/kotlin/com/advanced_baseball_stats/handler/batting/TotalBattingStatHandler.kt
+++ src/main/kotlin/com/advanced_baseball_stats/handler/batting/TotalBattingStatHandler.kt
@@ -3,23 +3,15 @@ package com.advanced_baseball_stats.handler.batting
 import com.advanced_baseball_stats.model.batting.BattingGameStat
 import com.advanced_baseball_stats.model.batting.BattingStat
 import com.advanced_baseball_stats.repository.PlaysSql
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import com.advanced_baseball_stats.utility.converter.date.DateHelper
 
 class TotalBattingStatHandler
 {
-    fun getCurrentDate(): String
-    {
-        val curDate             = LocalDate.now()
-        val dateTimeFormatter   = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-
-        return dateTimeFormatter.format(curDate)
-    }
-
     fun getStats(id: String, pitcherId: String, pitcherHandedness: String, startDate: String, endDate: String, statList: List<BattingStat>): List<BattingGameStat>
     {
-        val finalEndDate = if (endDate.isEmpty()) this.getCurrentDate() else endDate
+        val finalEndDate = if (endDate.isEmpty()) DateHelper.getCurrentDate() else endDate
 
+        //TODO: Add equivalent function in playerbattingsql and swap based on if pitcher info is provided
         return PlaysSql.getBattingStatsTotalByPitcher(id, pitcherId, pitcherHandedness, startDate, finalEndDate, statList)
     }
 }
