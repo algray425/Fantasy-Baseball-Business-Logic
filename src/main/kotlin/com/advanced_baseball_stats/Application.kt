1c6f51e821858a07e96971edf353649091f9a0ee
algray425 <16615934+algray425@users.noreply.github.com>
2025-09-18 13:31:15 -0700

    Dependency Injection for GradeHandler
    Changing GradeHandler from a singleton to use dependency injection pattern.

--- src/main/kotlin/com/advanced_baseball_stats/Application.kt
+++ src/main/kotlin/com/advanced_baseball_stats/Application.kt
@@ -4,6 +4,10 @@ import com.advanced_baseball_stats.handler.batting.AggregateBattingStatHandler
 import com.advanced_baseball_stats.handler.batting.BattingStatHandler
 import com.advanced_baseball_stats.handler.batting.PerGameBattingStatHandler
 import com.advanced_baseball_stats.handler.batting.TotalBattingStatHandler
+import com.advanced_baseball_stats.handler.grade.AggregatePercentileGradesHandler
+import com.advanced_baseball_stats.handler.grade.AggregatePlayerGradesHandler
+import com.advanced_baseball_stats.handler.grade.GradeHandler
+import com.advanced_baseball_stats.handler.grade.PerGamePercentileGradesHandler
 import com.advanced_baseball_stats.handler.pitching.AggregatePitchingStatHandler
 import com.advanced_baseball_stats.handler.pitching.PerGamePitchingStatHandler
 import com.advanced_baseball_stats.handler.pitching.PitchingStatHandler
@@ -26,7 +30,13 @@ fun Application.module()
 
     val pitchingStatHandler = PitchingStatHandler(perGamePitchingStatHandler, aggregatePitchingStatHandler)
 
+    val perGamePercentileGradesHandler      = PerGamePercentileGradesHandler    ()
+    val aggregatePercentileGradesHandler    = AggregatePercentileGradesHandler  ()
+    val aggregatePlayerGradesHandler        = AggregatePlayerGradesHandler      ()
+
+    val gradeHandler = GradeHandler(perGamePercentileGradesHandler, aggregatePercentileGradesHandler, aggregatePlayerGradesHandler)
+
     configureSerialization  ()
     configureCors           ()
-    configureRouting        (battingStatHandler, pitchingStatHandler)
+    configureRouting        (battingStatHandler, pitchingStatHandler, gradeHandler)
 }
